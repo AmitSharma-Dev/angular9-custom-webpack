7b36ff1762d4715a196d30c501ec83a696fa06ee
amits-dev <amsdev232@gmail.com>
2020-03-09 11:28:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+declare const APP_VERSION: number;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-builder-testing';
+  title = 'angular-builder-testing ' + APP_VERSION;
 }
